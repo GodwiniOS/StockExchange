756cc9c0999aae3d60789f852025c09b0eadd895
Godwin A <iamgodwin10@gmail.com>
2021-12-09 22:23:28 +0400

    Initial Commit

---
+++ StockExchange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockExchange
+//
+//  Created by Godwin A on 09/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
